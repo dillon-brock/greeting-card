56efbc64d6d23f4eb453d1dec025e7432dd9d31a
Dillon Brock <mdillon1297@gmail.com>
2022-06-02 21:57:29 -0700

    working card title changes on input

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 // grab DOM elements
 
+const titleInput = document.getElementById('title-input');
+const titleDisplay = document.getElementById('title-display');
+
 // set event listeners
     // get info from user input
     // use user input to update state
     // update DOM to reflect the new state
+
+titleInput.addEventListener('input', () => {
+    titleDisplay.textContent = titleInput.value;
+});
